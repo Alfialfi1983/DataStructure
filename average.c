76f8fcc0697410897ecb08a031f66c47bb938483
Alfialfi1983 <117815598+Alfialfi1983@users.noreply.github.com>
2023-01-05 11:50:25 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+float avg;
+printf("enter three numbers:");
+scanf("%d%d%d",&a,&b,&c);
+avg=(a+b+c)/3;
+printf("the average of three numbers is %f",avg);
+}
+
